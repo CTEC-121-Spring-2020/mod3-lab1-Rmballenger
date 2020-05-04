22fe5834c0b8d8a95b82b1a9bcb2ff32efb29592
Rmballenger <r.ballenger@students.clark.edu>
2020-05-04 11:42:09 -0700

    Pushing before restarting of computer.

---
+++ Module3Lab1.py
@@ -0,0 +1,83 @@
+"""
+CTEC 121
+Robert Ballenger
+Module 3 Lab 1
+<assignment/lab description>
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    # conditional expressions
+
+    # literal
+    print("\nBoolean literals")
+    print(True)
+    print(False)
+    print(type(True))
+    print()
+
+    # relational operatiors
+    print("Relational operators")
+    print("3 < 5", 3 < 5)
+    print("3 <= 3", 3 <= 3)
+    print("3 == 3", 3 == 3)
+    print("3 >= 5", 3 >= 5)
+    print("3 != 5", 3 != 5)
+    print()
+
+    # using variables
+    x = 3
+    y = 5
+    print("Using variables")
+    print("x:", x, "y:", y)
+    print("x < y", x < y)
+    print("x != y", x != y)
+    print()
+
+    # simple if statements
+    print("x:", x, "y:", y)
+    if x < y:
+        print("x < y")
+    
+    if x > y:
+        print("x > y")
+    
+    print("end simple if example")
+    print()
+
+    # if/else statement
+    print("if/else statement")
+    print("x:", x, "y:", y)
+    if x < y:
+        print("x < y")
+    else:
+        print("x >= y")
+
+    x = 6 
+    print("x:", x, "y:", y)
+    if x < y:
+        print("x < y")
+    else:
+        print("x >= y")
+    
+    print("end if/else example")
+    print()
+
+    # Exercise 3-1
+    # Have a program count from 1 to 10.
+    # For each number, print a line that gives the number
+    # and identifies whether the number is odd or even.
+
+
+    for i in range(1, 11, 1):
+        if (i % 2) == 0:
+            print(i, "is even")
+        else:
+            print(i, "is odd")
+
+main()    
